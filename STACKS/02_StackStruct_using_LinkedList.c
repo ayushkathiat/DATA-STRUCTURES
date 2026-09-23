842cbf91d5de2ac723576da9533d45f7da2307a8
Ayush Kathait <ayushkathait009@gmail.com>
2026-09-23 18:00:22 +0530

    Ubdated

--- STACKS/02_StackStruct_using_LinkedList.c
+++ STACKS/02_StackStruct_using_LinkedList.c
@@ -1,80 +1,88 @@
-#include <stdio.h>
+#include <Stdio.h>
 #include <stdlib.h>
-struct Node{
-    int data;
-    struct stack *next;
-    int size;
-    int top;
-    int *arr;
-};
 
-struct Node *push(struct Node *s, int value)
+struct Node
 {
-    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
-    if(s->top == s->size-1){
-        printf("Stack is Overflow");
-    }
-    else{
-        n->data = s;
-        n->next = s->top;
-        s->top = n;
+    int data;
+    struct Node *next;
+};
+struct Node* top = NULL;
+
+
+void StackTraversal(struct Node *ptr){
+    while(ptr != NULL){
+        printf("Ele is : %d\n", ptr->data);
+        ptr = ptr->next;
     }
 }
 
-struct Node *pop(struct Node *s, int value)
-{
-    if(s->top == NULL){
-        printf("Stack is Empty!");
+struct Node* push(struct Node *top, int s){
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    if(n == NULL){
+        printf("Stack is overflow!");
+    }else{
+        n->data = s;
+        n->next = top;
+        top = n;
+    }
+}
+
+int pop(struct Node* tp ){
+    if(tp == NULL){
+        printf("Stack is Underflow!");
+    }else{
+    struct Node *n = tp;
+        top = tp->next;
+        int x = n->data;
+        free(n);
+        return x;
+    }
+} 
+
+int peek(struct Node *top){
+    if(top == NULL){
+        printf("stack is Underflow!");
+        return -1;
+    }else{
+        struct Node *ptr = top;
+        int i = 0;
+        while(i < pos-1 && ptr != NULL){
+            i++;
+        }
+        
+        top = top->data;
+
+
+    }
+
+}
+
+int isEmpty(struct Node *top){
+    if(top == NULL){
+        printf("Stack is empty");
         return 1;
     }
-    else{
-        printf("Poped sucessfullt : %d\n", value);
-        s->top--;
-    }
+    else return 0;
 }
 
+int isFull(struct Node *top){
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    if(n == NULL){
+        printf("Stack is FUll!");
+        return 1;
+    }
+    else return 0;
+}
 
 int main(){
-    struct Node *s = (struct Node *)malloc(sizeof(struct Node));
+    top = push(top, 10);
+    top = push(top, 20);
+    top = push(top, 30);
+    StackTraversal(top);
 
-    printf("Enter the size of stack : ");
-    scanf("%d", s->size);
-    s->top = -1;
-    s->arr = (int*)malloc(s->size * sizeof(int));
+    int element = pop(top);
+    printf("Poped ele is : %d\n", element);
+    StackTraversal(top);
 
-    int value, choice;
-    do{
-       printf("Enter the choice : ");
-       scanf("%d", choice);
-       printf("\n1. push\n2. pop\n3. peek\n4. isEmpty\n5. isFull\n6. Display\n7. Exit\n");
-       switch (choice){
-          case 1:
-        printf("Enter the number you want to push : ");
-        scanf("%d", value);
-        push(s, value);
-        break;
-          case 2:
-        pop(s);
-        break;
-         case 3:
-        peek(s);
-        break;
-    case 4:
-        isEmpty(s);
-        break;
-    case 5:
-        isFull(s);
-        break;
-    case 6:
-        Dsplay(s);
-        break;
-    case 7:
-        printf("Exiting\n");
-        break;
-    default:
-        printf("\nInvalid choice");
-    }
-    while(choice != 7);
-    }
     return 0;
 }
